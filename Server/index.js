12c19c5d4a69072bdb7c23095c2246d5f2fe3383
badhusha <muhammadbadhusha7@gmail.com>
2025-04-07 23:20:52 +0530

    fix db

--- Server/index.js
+++ Server/index.js
@@ -4,12 +4,10 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const TodoModel = require('./Models/todos')
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+const mongoURl=process.env.MONGODB_URL
+mongoose.connect(mongoURl)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 const app = express()
 app.use(cors())
